a927ae7313d4de7aeff7b9d19d1359ce0740ad79
Maxwell Lin <ml7013@princeton.edu>
2022-12-15 15:44:43 -0500

    fixed b

--- miniassembler.c
+++ miniassembler.c
@@ -122,7 +122,7 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    /* Base Instruction Code */
    uiInstr = 0x14000000;
 
-   /* displacement to be split into immlo and immhi and inserted */
+   /* THis one is still weird for some reason */
    uiDisp = (unsigned int)(ulAddr - ulAddrOfThisInstr);
    uiDisp = uiDisp >> 2;
    setField(uiDisp, 0, &uiInstr, 0, 26);
